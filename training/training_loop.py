c6f0c92656fe4bebb3efcab083b78a3e9709875a
Vojtech Semecky <vojtech@semecky.cz>
2022-01-19 15:24:16 +0100

    Refactoring: Simplify save_image_grid()

--- training/training_loop.py
+++ training/training_loop.py
@@ -69,22 +69,8 @@ def setup_snapshot_image_grid(training_set, random_seed=0):
 #----------------------------------------------------------------------------
 
 def save_image_grid(img, fname, drange, grid_size):
-    lo, hi = drange
-    img = np.asarray(img, dtype=np.float32)
-    img = (img - lo) * (255 / (hi - lo))
-    img = np.rint(img).clip(0, 255).astype(np.uint8)
-
-    gw, gh = grid_size
-    _N, C, H, W = img.shape
-    img = img.reshape([gh, gw, C, H, W])
-    img = img.transpose(0, 3, 1, 4, 2)
-    img = img.reshape([gh * H, gw * W, C])
-
-    assert C in [1, 3]
-    if C == 1:
-        PIL.Image.fromarray(img[:, :, 0], 'L').save(fname)
-    if C == 3:
-        PIL.Image.fromarray(img, 'RGB').save(fname)
+    grid_pil = get_image_grid(img, drange, grid_size)
+    grid_pil.save(fname)
 
 #----------------------------------------------------------------------------
 
