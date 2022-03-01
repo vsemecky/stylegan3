7eff93fb08e18c87ea69c0919550b86270d1ccc2
Vojtech Semecky <vojtech@semecky.cz>
2022-03-01 17:58:38 +0100

    DD: Fix

--- train.py
+++ train.py
@@ -74,7 +74,7 @@ def launch_training(c, desc, outdir, dry_run):
     print(f'Dataset path:        {c.training_set_kwargs.path}')
     print(f'Dataset size:        {c.training_set_kwargs.max_size} images')
     print(f'Dataset resolution:  {c.training_set_kwargs.resolution}')
-    print(f'Dataset labels:      {c.training_set_kwargs._use_labels}')
+    print(f'Dataset labels:      {c.training_set_kwargs.use_labels}')
     print(f'Dataset x-flips:     {c.training_set_kwargs.xflip}')
     print()
 
